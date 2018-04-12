f6384f87581b9d80ecb05077fd1a97eea811c300
Andrii Kovalchuk <tieorange@gmail.com>
2018-04-12 14:24:56 +0200

    Initial Commit

---
+++ iotSidebarWidget2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iotSidebarWidget2
+//
+//  Created by Andrii Kovalchuk on 12/04/2018.
+//  Copyright © 2018 Andrii Kovalchuk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iotSidebarWidget2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iotSidebarWidget2
+//
+//  Created by Andrii Kovalchuk on 12/04/2018.
+//  Copyright © 2018 Andrii Kovalchuk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
